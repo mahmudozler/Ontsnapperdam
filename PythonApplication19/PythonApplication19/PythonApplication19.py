236f57e83b50366db02c91cc72b4a3d7b4610ebf
mahmudozler <mahmutozler96@gmail.com>
2017-01-18 13:33:47 +0100

    pawns
    pawns

--- PythonApplication19/PythonApplication19/PythonApplication19.py
+++ PythonApplication19/PythonApplication19/PythonApplication19.py
@@ -2,7 +2,7 @@ import pygame
 
 size =(600,700)
 screen = pygame.display.set_mode(size)
-screen.fill((250,250,0))
+screen.fill((250,250,250))
 
 done = False
 
@@ -22,10 +22,20 @@ while not done:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			done = True
-	pygame.draw.circle(screen,(0,0,0),(30,30),15)
+	
 
 	player1=Player((137,237,75),100,100,20)
 	player1.draw(screen)
+	player2=Player((255,255,0),150,100,20)
+	player2.draw(screen)
+	player3=Player((91,183,211),200,100,20)
+	player3.draw(screen)
+	player4=Player((116,59,124),250,100,20)
+	player4.draw(screen)
+	player5=Player((237,65,56),300,100,20)
+	player5.draw(screen)
+	player6=Player((0,0,0),350,100,20)
+	player6.draw(screen)
 
 	pygame.display.flip()
 
