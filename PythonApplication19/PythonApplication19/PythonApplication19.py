aad7041aa202dcf87a55395e37ee2625ff27fd2d
Satrya Sabeni <s.sabeni17@gmail.com>
2017-01-19 11:06:21 +0100

    update(move pawn) function added

--- PythonApplication19/PythonApplication19/PythonApplication19.py
+++ PythonApplication19/PythonApplication19/PythonApplication19.py
@@ -22,55 +22,50 @@ class Player:
 		self.y = y
 		self.r = r
 
-	"""def update(self):
-		keys = pygame.key.get_pressed()
-		if keys[pygame.K_LEFT]:
-			self.x -= 10"""
-
-
 	def draw(self,screen):
 		pygame.draw.circle(screen,self.kleur,(int(self.x),int(self.y)),int(self.r))
 
-player1 = Player((137, 237, 75), 300, 30, 13)
+def Update(key,player):
+	if key == pygame.K_LEFT:
+		screen.fill((250, 250, 250))
+		player.x -= 40
+		player.draw(screen)
+	elif key == pygame.K_RIGHT:
+		screen.fill((250, 250, 250))
+		player.x += 40
+		player.draw(screen)
+	elif key == pygame.K_UP:
+		screen.fill((250, 250, 250))
+		player.y -= 40
+		player.draw(screen)
+	elif key == pygame.K_DOWN:
+		screen.fill((250, 250, 250))
+		player.y += 40
+		player.draw(screen)
+
+
+player1 = Player((155,255,140),300,30,13)
+player2 = Player((155,255,140),340,30,13)
+player3 = Player((91,183,211),380,30,13)
+player4 = Player((116,59,124),420,30,13)
+player5 = Player((237,65,56),460,30,13)
+player6 = Player((0,0,0),500,30,13)
 
 while not done:
-	#player1 = Player((137, 237, 75), 100, 100, 20)
-	player1.draw(screen)
-
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			done = True
 		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_LEFT:
-				screen.fill((250,250,250))
-				player1.x -= 50
-				player1.draw(screen)
-			elif event.key == pygame.K_RIGHT:
-				screen.fill((250, 250, 250))
-				player1.x += 50
-				player1.draw(screen)
-			elif event.key == pygame.K_UP:
-				screen.fill((250, 250, 250))
-				player1.y -= 50
-				player1.draw(screen)
-			elif event.key == pygame.K_DOWN:
-				screen.fill((250, 250, 250))
-				player1.y += 50
-				player1.draw(screen)
+			Update(event.key,player3)
 
-	player2=Player((155,255,140),340,30,13)
+
+	#player draw
+	player1.draw(screen)
 	player2.draw(screen)
-	player3=Player((91,183,211),380,30,13)
 	player3.draw(screen)
-	player4=Player((116,59,124),420,30,13)
 	player4.draw(screen)
-	player5=Player((237,65,56),460,30,13)
 	player5.draw(screen)
-	player6=Player((0,0,0),500,30,13)
 	player6.draw(screen)
-		
-
-	
 
 	pygame.display.flip()
 
