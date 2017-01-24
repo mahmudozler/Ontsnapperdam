8ffa6e63af9e8d8e4d6914f42ee26f8de94fd4b6
mikekeehnen <mikekeehnen@me.com>
2017-01-24 12:23:45 +0100

    een paar kleine updates

--- ontsnapperdammenu.py
+++ ontsnapperdammenu.py
@@ -7,11 +7,15 @@ import random
 
 pygame.init()
 width = 800
-height = 600
-img = pygame.image.load('start.png')
+height = 850
+img = pygame.image.load('play.png')
 img3 = pygame.image.load('exit.png')
+bg = pygame.image.load('speelbord.png')
+ologo = pygame.image.load('ontsnapperdam logo.png')
 img2 = pygame.transform.smoothscale(img,(200, 70))
 img4 = pygame.transform.smoothscale(img3,(200,70))
+bg1 = pygame.transform.smoothscale(bg,(width,height))
+ologo1 = pygame.transform.smoothscale(ologo,(400,80))
 screen = pygame.display.set_mode((width,height))
 
 
@@ -26,34 +30,37 @@ done = True
 pygame.display.set_caption('Ontsnapperdam')
 screen.fill(white)
 
+
 defaultfont = pygame.font.get_default_font()
 fontrenderer = pygame.font.Font(defaultfont,85)
 
 label = fontrenderer.render("Ontsnapperdam",1,red)
 label2 = fontrenderer.render("Settings",1,black)
-screen.blit(label,(40,15))
+
 
 class start:
     def __init__(self):
         self.running = True
-        self.done = True
+
 
 
 
     def intro(self):
 
         while self.running:
+            screen.blit(bg1,(0,0))
             screen.blit(img2,(width*0.35,height*0.35))
             screen.blit(img4,(width*0.35,height*0.55))
+            screen.blit(ologo1,(15,15))
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
                     Game(players)
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     mousex, mousey = pygame.mouse.get_pos()
-                    if 300 <= mousex and 500 >= mousex and 250 <= mousey and 320 >= mousey:
+                    if  width*0.35 <= mousex and  width*0.60 >= mousex and height*0.35 <=mousey and height*0.466 >= mousey:
                         self.running = False
-                    elif 300 <= mousex and 500 >= mousex and 350 <= mousey and 420 >= mousey:
+                    elif width*0.35 <= mousex and width*0.60 >= mousex and height*0.55 <= mousey and height*0.766 >= mousey:
                         pygame.quit()
                         quit()
 
@@ -62,105 +69,183 @@ class start:
 
 menu = start()
 
+done = False
 
 
 class Player:
-    def __init__(self,name,kleur,x,y,r):
+    def __init__(self, name, kleur, x, y):
         self.name = name
         self.kleur = kleur
         self.x = x
         self.y = y
-        self.r = r
-        self.counter = 0
+        self.r = 10
+        self.rect = pygame.Rect(self.x, self.y, 20, 20)
+        self.steps = 0
 
-    def draw(self,screen):
-        pygame.draw.circle(screen,self.kleur,(int(self.x),int(self.y)),int(self.r))
+    def draw(self, screen):
+        pygame.draw.circle(screen, self.kleur, (self.rect.center), self.r)
 
-    def Update(self):
+    def Update(self, screen, event, blocks):
         if event.type == pygame.KEYDOWN:
+            # to check the new position is within the game blocks
+            newpos = self.rect.copy()
+
             if event.key == pygame.K_LEFT:
-                screen.fill((250, 250, 250))
-                self.x -= 40
-                self.draw(screen)
+                newpos.x -= 26
             elif event.key == pygame.K_RIGHT:
-                screen.fill((250, 250, 250))
-                self.x += 40
-                self.draw(screen)
+                newpos.x += 26
             elif event.key == pygame.K_UP:
-                screen.fill((250, 250, 250))
-                self.y -= 40
-                self.draw(screen)
+                newpos.y -= 26
             elif event.key == pygame.K_DOWN:
-                screen.fill((250, 250, 250))
-                self.y += 40
-                self.draw(screen)
+                newpos.y += 26
+
+            # bool check is newpos is inside game rectangles
+            for rectangle in blocks:
+                if newpos.colliderect(rectangle):
+                    # convert newpos in the new position
+                    self.steps += 1
+                    self.rect = newpos
+                    # stop check when matched
+                    break
+
+    def Pos(self):
+        return self.x, self.y
+
 
 class Game:
-    def __init__(self,players):
+    def __init__(self, players):
+        pygame.init()
         self.turn = 0
-        self.steps = 0
         self.players = players
         self.thrown = 0
+        self.size = (800, 850)
+        self.running = False
 
-    def Update(self,event):
+        # block width,height,margin
+        self.w = 25
+        self.h = 25
+        self.m = 1
+
+        # colors
+        self.red = (191, 36, 36)
+        self.black = (23, 20, 20)
+
+        # self.screen = pygame.display.set_mode(self.size)
+        self.blocks = []
+        self.battleblocks = []
+
+        # Create list with all block position in the game
+        for row in range(30):
+            for col in range(30):
+                if self.Filter(col, row) == "battleblock":
+                    self.battleblocks.append(
+                        pygame.Rect((self.w + self.m) * col + self.m + 40, ((self.h + self.m) * row + self.m + 50),
+                                    self.w, self.h))
+                elif self.Filter(col, row) == "normalblock":
+                    self.blocks.append(
+                        pygame.Rect((self.w + self.m) * col + self.m + 40, ((self.h + self.m) * row + self.m + 50),
+                                    self.w, self.h))
+
+    def Update(self, event):
         player = self.players[self.turn]
         if event.type == pygame.KEYDOWN:
-            #Add arrow key constraints
-            self.steps += 1
-            player.Update()
+            player.Update(self.screen, event, self.blocks)
 
-            #if all steps made
-            if self.steps == self.thrown:
+            # if all steps made
+            if player.steps == self.thrown:
                 self.thrown = 0
-                print("next dice")
-                if self.turn == 5:
+                print(player.Pos())
+                if self.turn == (len(self.players) - 1):
                     self.turn = 0
                 else:
                     self.turn += 1
-                self.steps = 0
+                player.steps = 0
 
-player1 = Player("A",(155,255,140),300,30,13)
-player2 = Player("B",(155,255,140),340,30,13)
-player3 = Player("C",(91,183,211),380,30,13)
-player4 = Player("D",(116,59,124),420,30,13)
-player5 = Player("E",(237,65,56),460,30,13)
-player6 = Player("F",(0,0,0),500,30,13)
+    def Filter(self, x, y):
+        map_list = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19], [0, 9, 19], [0, 9, 10, 19],
+                    [0, 10, 19], [0, 10, 19],
+                    [0, 1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 19], [0, 2, 4, 8, 13, 17, 18, 19],
+                    [0, 2, 3, 4, 8, 13, 17, 19], [0, 4, 8, 11, 12, 13, 14, 15, 16, 17, 19], [0, 4, 8, 11, 19],
+                    [0, 4, 5, 6, 7, 8, 11, 13, 14, 15, 16, 17, 18, 19], [0, 6, 11, 12, 13, 16, 19],
+                    [0, 6, 13, 14, 15, 16, 19], [0, 3, 4, 5, 6, 13, 16, 19],
+                    [0, 3, 6, 7, 8, 9, 10, 11, 12, 13, 16, 17, 18, 19],
+                    [0, 1, 2, 3, 6, 13, 16, 19], [0, 3, 6, 13, 16, 19], [0, 3, 6, 7, 8, 9, 13, 14, 15, 16, 19],
+                    [0, 2, 3, 4, 5, 6, 9, 13, 19], [0, 2, 9, 13, 19], [0, 2, 9, 10, 11, 12, 13, 19],
+                    [0, 2, 3, 4, 5, 6, 13, 19], [0, 6, 13, 14, 15, 19],
+                    [0, 6, 15, 19], [0, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 19], [0, 1, 2, 3, 4, 5, 6, 8, 15, 19],
+                    [3, 8, 13, 14, 15, 19], [3, 8, 13, 19], [3, 8, 13, 19],
+                    [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]]
+        battle_blocks = [[], [], [], [0], [0], [0, 1], [0], [13, 17], [8, 13, 14, 15, 16, 17], [8], [7, 8], [],
+                         [13, 14], [3, 4, 5, 6, 13],
+                         [3, 6, 13], [13], [13], [13, 14], [], [], [], [], [], [], [], [3, 4, 5, 6], [3, 13], [13],
+                         [13], [11, 12, 13]]
+        if x in battle_blocks[y]:
+            return "battleblock"
+        elif x in map_list[y]:
+            return "normalblock"
+        return False
 
-players = [player1,player2,player3,player4,player5,player6]
+    def Draw(self):
+
+        # draw canvas
+        self.screen = pygame.display.set_mode(self.size)
+        self.screen.fill((255, 255, 255))
+
+        # draw dice
+        pygame.draw.rect(self.screen, (0, 0, 0), (10, 10, 50, 50), 1)
+
+        # draw all player
+        for player in self.players:
+            player.draw(self.screen)
+
+        # draw game board
+        for rectangle in self.blocks:
+            pygame.draw.rect(self.screen, self.black, rectangle, 1)
+        for rectangle in self.battleblocks:
+            pygame.draw.rect(self.screen, self.red, rectangle, 1)
+
+        # update whole screen
+        pygame.display.flip()
+
+    def Gameloop(self):
+
+        self.Draw()
+
+        while not self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = True
+
+                # If click on dice
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    mousex, mousey = pygame.mouse.get_pos()
+                    if mousex > 10 and mousex < 60 and mousey > 10 and mousey < 60:
+                        self.thrown = random.randint(1, 6)
+                        print(self.thrown)
+
+                # execute if dice is thrown
+                if event.type == pygame.KEYDOWN:
+                    if self.thrown > 0:
+                        self.Update(event)
+
+                        self.Draw()
+                        # pygame.display.flip()
+
+
+player1 = Player("A", (155, 255, 140), 200, 28)
+player2 = Player("B", (155, 255, 140), 226, 28)
+player3 = Player("C", (91, 183, 211), 252, 28)
+player4 = Player("D", (116, 59, 124), 278, 28)
+player5 = Player("E", (237, 65, 56), 304, 28)
+player6 = Player("F", (0, 0, 0), 330, 28)
+
+players = [player1, player2, player3, player4]
+
 game = Game(players)
-dice = 0
-
-while not done:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-        #click on dice to start
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            mousex, mousey = pygame.mouse.get_pos()
-            if mousex > 10 and mousex < 60 and mousey > 10 and mousey <60:
-                game.thrown = random.randint(1,6)
-                print(game.thrown)
-
-        #execute if dice is thrown
-        if event.type == pygame.KEYDOWN:
-            if game.thrown > 0:
-                game.Update(event)
-
-    #player draw initial position
-    player1.draw(screen)
-    player2.draw(screen)
-    player3.draw(screen)
-    player4.draw(screen)
-    player5.draw(screen)
-    player6.draw(screen)
-
-    # dice
-    pygame.draw.rect(screen, (0, 0, 0), (10, 10, 50, 50), 1)
-
-    pygame.display.flip()
 
 class Program:
     menu.intro()
+    game.Gameloop()
 
 
 Program()
