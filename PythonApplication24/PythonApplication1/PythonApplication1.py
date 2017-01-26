66db28a8ef075a758b2a0237fea1b2d777c67ce7
mahmudozler <mahmutozler96@gmail.com>
2017-01-26 11:26:21 +0100

    database higschore

---
+++ PythonApplication24/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,46 @@
+import psycopg2
+
+import math
+
+
+# Use the database
+def interact_with_database(command):
+    # Connect and set up cursor
+	connection = psycopg2.connect("host='localhost' dbname='postgres' user='postgres' password='2450086'")
+	cursor = connection.cursor()
+    
+    # Execute the command
+	cursor.execute(command)
+	connection.commit()
+
+    # Save results
+	results = None
+	try:
+		results = cursor.fetchall()
+	except psycopg2.ProgrammingError:
+        # Nothing to fetch
+		pass
+
+    # Close connection
+	cursor.close()
+	connection.close()
+    
+	return results
+
+
+# Uploads a score into the hiscore table
+def upload_score(name, score):
+	interact_with_database("UPDATE score SET score = {} WHERE name = '{}'".format(name , score))
+
+
+# Downloads score data from database
+def download_scores():
+	return interact_with_database("SELECT * FROM score")
+print(download_scores())
+
+# Downloads the top score from database
+def download_top_score():
+	result = interact_with_database("SELECT * FROM score ORDER BY score")[0][1]
+	return result
+
+#interact_with_database("SELECT * FROM users")
--- PythonApplication24/PythonApplication24/PythonApplication24.py
+++ PythonApplication24/PythonApplication24/PythonApplication24.py
@@ -1,16 +1,163 @@
+#imports
 import pygame
 from pygame.locals import *
 import sys
 import time
 import random
+import psycopg2
 
+
+# Use the database
+def interact_with_database(command):
+    # Connect and set up cursor
+    connection = psycopg2.connect("dbname=postgres user=postgres password='2450086'")
+    cursor = connection.cursor()
+    
+    # Execute the command
+    cursor.execute(command)
+    connection.commit()
+
+    # Save results
+    results = None
+    try:
+        results = cursor.fetchall()
+    except psycopg2.ProgrammingError:
+        # Nothing to fetch
+        pass
+
+    # Close connection
+    cursor.close()
+    connection.close()
+    
+    return results
+
+
+
+# Uploads a score into the hiscore table
+def upload_score(name, score):
+    interact_with_database("UPDATE users SET score = {} WHERE name = '{}'")
+
+
+# Downloads score data from database
+def download_scores():
+    return interact_with_database("SELECT * FROM users")
+
+
+# Downloads the top score from database
+def download_top_score():
+    result = interact_with_database("SELECT * FROM users ORDER BY name")[0][1]
+    return result
+
+
+
+
+
+pygame.init()
+width = 800
+height = 850
+
+screen = pygame.display.set_mode((width,height))
+
+#colors
+
+black = (0,0,0)
+red = (255,0,0)
+white = (255,255,255)
+grey = (158,158,158)
+done = True
+
+
+#highscore screen---
+
+pygame.display.set_caption('HighScore')
+
+
+defaultfont = pygame.font.get_default_font()
+fontrenderer = pygame.font.Font(defaultfont,85)
+
+#highscorelabel
+label3 = fontrenderer.render("HighScore",1,black)
+screen.blit(label3,(200,15))
+
+#select *from users func
+score = download_scores()
+
+#print table of users and score
+def table():
+	buffer=[] 
+	for i in score:
+		x = "{} --> {}".format(i[0],i[1])
+		buffer.append(x)
+
+	#for x in buffer:
+		#print(x)
+	return buffer
+	
+list = table()
+
+
+
+#data from database
+font = pygame.font.SysFont(None, 40)
+
+
+#---------------------------------------------------------------------------
+
+#gameboard background
+bg = pygame.image.load('speelbord.png')
+bg1 = pygame.transform.smoothscale(bg,(width,height))
+screen = pygame.display.set_mode((width,height))
+
+#back button
+img5 = pygame.image.load('back.png')
+img6 = pygame.transform.smoothscale(img5,(100, 100))
+
+defaultfont = pygame.font.get_default_font()
+fontrenderer = pygame.font.Font(defaultfont,85)
+
+class hsscreen:
+	def __init__(self):
+		self.running = True
+
+
+
+
+	def introo(self):
+
+		while self.running:
+			screen.blit(bg1,(0,0))
+			screen.blit(label3,(200,15))
+			#screen.blit(score_text, (100, 175))
+			screen.blit(img6,(50,25))
+
+			#list
+			count = 0
+			for s in list:
+				count += 1
+				screen.blit(font.render("{}".format(s),1,black),(250 ,(150 + (100 * count))))
+
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					self.running = False
+					Game(players)
+				if event.type == pygame.MOUSEBUTTONDOWN:
+					mousex, mousey = pygame.mouse.get_pos()
+					if mousex > 50 and mousex < 150 and mousey > 25 and mousey < 125:
+						self.running = False
+						
+
+			pygame.display.flip()
+
+
+hs = hsscreen()
+
+done = False
 
 pygame.init()
 width = 800
 height = 850
 img = pygame.image.load('play.png')
 img3 = pygame.image.load('exit.png')
-bg = pygame.image.load('speelbord.png')
 ologo = pygame.image.load('ontsnapperdam logo.png')
 img2 = pygame.transform.smoothscale(img,(200, 70))
 img4 = pygame.transform.smoothscale(img3,(200,70))
@@ -18,17 +165,6 @@ bg1 = pygame.transform.smoothscale(bg,(width,height))
 ologo1 = pygame.transform.smoothscale(ologo,(400,80))
 screen = pygame.display.set_mode((width,height))
 
-
-
-
-black = (0,0,0)
-red = (255,0,0)
-white = (255,255,255)
-done = True
-
-
-pygame.display.set_caption('Ontsnapperdam')
-screen.fill(white)
 
 
 defaultfont = pygame.font.get_default_font()
@@ -39,35 +175,35 @@ label2 = fontrenderer.render("Settings",1,black)
 
 
 class start:
-    def __init__(self):
-        self.running = True
+	def __init__(self):
+		self.running = True
 
 
 
 
-    def intro(self):
+	def intro(self):
 
-        while self.running:
-            screen.blit(bg1,(0,0))
-            screen.blit(img2,(width*0.35,height*0.35))
-            screen.blit(img4,(width*0.35,height*0.55))
-            screen.blit(ologo1,(15,15))
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running = False
-                    Game(players)
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    mousex, mousey = pygame.mouse.get_pos()
-                    if  width*0.35 <= mousex and  width*0.60 >= mousex and height*0.35 <=mousey and height*0.466 >= mousey:
-                        self.running = False
-                    elif width*0.35 <= mousex and width*0.60 >= mousex and height*0.55 <= mousey and height*0.766 >= mousey:
-                        pygame.quit()
-                        quit()
+		while self.running:
+			screen.blit(bg1,(0,0))
+			screen.blit(img2,(width*0.35,height*0.35))
+			screen.blit(img4,(width*0.35,height*0.55))
+			screen.blit(ologo1,(15,15))
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					self.running = False
+					Game(players)
+				if event.type == pygame.MOUSEBUTTONDOWN:
+					mousex, mousey = pygame.mouse.get_pos()
+					if  width*0.35 <= mousex and  width*0.60 >= mousex and height*0.35 <=mousey and height*0.466 >= mousey:
+						self.running = False
+					elif width*0.35 <= mousex and width*0.60 >= mousex and height*0.55 <= mousey and height*0.766 >= mousey:
+						pygame.quit()
+						quit()
 
-            pygame.display.flip()
+			pygame.display.flip()
 
 
-menu = menu=start()
+menu=start()
 
 done = False
 
@@ -244,13 +380,9 @@ players = [player1, player2, player3, player4]
 game = Game(players)
 
 class Program:
-    menu.intro()
-    game.Gameloop()
+	menu.intro()
+	hs.introo()
+	game.Gameloop()
 
 
 Program()
-
-
-
-
-
